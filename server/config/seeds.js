354df6519af6d04225987e015776223d1316df18
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-08-25 19:46:15 -0700

    seed has all categories, menu items, descriptions, and price

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -19,53 +19,48 @@ db.once('open', async () => {
     {
       name: 'Nachos',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+        'Chips with melted cheese, salsa, sour cream, guacamole, and your choice of meet',
       image: 'cookie-tin.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 15.99
     },
     {
       name: 'Chips & Salsa',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+        'Our house-made tortilla chips along with our house-made salsa',
       image: 'cookie-tin.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 6.99
     },
     {
       name: 'Quesadilla',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+        'Cheese quesadilla with salsa and sour cream on the side',
       image: 'canned-coffee.jpg',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      price: 7.49
     },
     {
       name: 'Burrito',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
+        'Mexican rice, black beans, salsa, sour cream, guacamole, cheese, and your choice of meat',
       image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
+      price: 15.99
     },
     {
       name: 'Tacos',
       category: categories[1]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
+        'Three authentic mexican tacos with your choice of meat',
       image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
+      price: 14.49,
     },
     {
       name: 'Fajitas',
       category: categories[1]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+        'Cooked peppers and onions and your choice of chicken or beef along with two tortillas',
       image: 'wooden-spoons.jpg',
       price: 14.99,
       quantity: 100
@@ -74,63 +69,56 @@ db.once('open', async () => {
       name: 'Tamales',
       category: categories[1]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+        'Two of our special authenic made tamales!',
       image: 'wooden-spoons.jpg',
       price: 14.99,
-      quantity: 100
     },
     {
       name: 'Horchata',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
+        'A traditional mexican beverage',
       image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+      price: 5.49,
     },
     {
       name: 'Jarritos',
       category: categories[2]._id,
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
+        'Mexican Soda',
       image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+      price: 2.49,
     },
     {
       name: 'Margarita',
       category: categories[2]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+        'Margarita with top-shelf tequila',
       image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
+      price: 11.99,
     },
     {
       name: 'Tres Leches Cake',
       category: categories[3]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
+      description: 'Slice of Three Layered Cake',
       image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
+      price: 7.49,
     },
     {
       name: 'Churros',
       category: categories[3]._id,
       description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
+        '6 Mini Churros',
       image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
+      price: 3.49,
     },
     {
       name: 'Caramel Flan',
       category: categories[3]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
+        'Custard dessert with a layer of caramel at the top',
       image: 'teddy-bear.jpg',
       price: 7.99,
-      quantity: 100
     }
   ]);
 
