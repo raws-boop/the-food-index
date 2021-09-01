bed2b3f7385d1171d8e855773ddbfbc4705ffab6
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-08-31 19:58:02 -0700

    removed quantity

--- server/models/MenuItem.js
+++ server/models/MenuItem.js
@@ -19,11 +19,6 @@ const menuItemSchema = new Schema({
     required: true,
     min: 0.99
   },
-  quantity: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
   category: {
     type: Schema.Types.ObjectId,
     ref: 'Restaurant',
