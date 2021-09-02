bfe07deb9ffb97302703d7b6ce0a3f3e1753daf5
Selma Burke <selmasburke@gmail.com>
2021-09-02 00:48:13 -0700

    fixed cart & item detail errors, updated UI

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
@@ -47,12 +46,11 @@ function App() {
             <Nav />
             <Switch>
               <Route exact path="/" component={Dash} />
-              <Route exact path="/home" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/menuItems/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
